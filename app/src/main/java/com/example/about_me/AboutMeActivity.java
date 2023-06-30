62adea224cd1ad254f28bdafb2c3a4ccba760ebd
mimarchant <mmarchant@moveapps.cl>
2023-06-29 22:44:52 -0400

    se termina layout landscape y se agregan intents

--- app/src/main/java/com/example/about_me/AboutMeActivity.java
+++ app/src/main/java/com/example/about_me/AboutMeActivity.java
@@ -2,13 +2,94 @@ package com.example.about_me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.about_me.databinding.ActivityAboutMeBinding;
 
 public class AboutMeActivity extends AppCompatActivity {
+    ActivityAboutMeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_me);
+        binding = ActivityAboutMeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWhatsApp();
+            }
+        });
+
+        binding.emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+
+        binding.linkedinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLinkedin();
+            }
+        });
+
+        binding.callmeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callMe();
+            }
+        });
     }
+
+    private void openWhatsApp() {
+        // Creación de método con intent implícito para enviar mensaje por Whatsapp.
+
+            String contact = "+56934332449";
+            String url = "https://api.whatsapp.com/send?phone="+ contact +"&text= Holi, que ondi";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+    }
+
+    private void sendEmail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mimarchtt@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hola Miguel :D");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send email..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getBaseContext(), "No hay un cliente de correos instalado.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showLinkedin() {
+        String url = "https://www.linkedin.com/in/miguel-marchant/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        try {
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getBaseContext(), "No application can handle this request. Please install a web browser or check your URL.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void callMe() {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:+56934332449"));
+        startActivity(callIntent);
+    }
+
+
 }
--- app/src/main/java/com/example/about_me/MainActivity.java
+++ app/src/main/java/com/example/about_me/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.about_me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         binding.followBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "Gracias por el follow <3", Toast.LENGTH_LONG).show();
+              followMe();
             }
         });
 
@@ -32,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void followMe() {
+        String url = "https://www.instagram.com/mimarchantt/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        try {
+            startActivity(i);
+            Toast.makeText(getBaseContext(), "Gracias por el follow <3", Toast.LENGTH_LONG).show();
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getBaseContext(), "No application can handle this request. Please install a web browser or check your URL.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
